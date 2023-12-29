9caa61dd7b7c4e2e047ccb9145934bb8f2c27f5b
hogesako <hogesako@ikasako.com>
2023-12-29 23:07:50 +0900

    fastembed

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
-from sentence_transformers import SentenceTransformer
+from fastembed.embedding import FlagEmbedding as Embedding
+from typing import List
+import numpy as np
+
 from flask import Flask, request
 from waitress import serve
 from werkzeug.exceptions import BadRequest
 import os
 
+model_loaded = False
 app = Flask(__name__)
-model_name = os.environ.get('MODEL_NAME', 'intfloat/multilingual-e5-small')
+model_name = os.environ.get('MODEL_NAME', 'intfloat/multilingual-e5-large')
 
 print("start model loading")
-model = SentenceTransformer(model_name)
+embedding_model = Embedding(model_name, max_length=512) 
+model_loaded = True
 print("finish model loading")
 
 @app.route('/', methods=["GET"])
@@ -20,7 +25,11 @@ def vectorize():
     sentence = request.json['sentence']
     if sentence == None:
         raise BadRequest
-    return model.encode(sentence, normalize_embeddings=True).tolist()
+    if model_loaded == False:
+        raise BadRequest
+    documents: List[str] = [sentence]
+    embeddings: List[np.ndarray] = list(embedding_model.embed(documents))
+    return embeddings[0].tolist()
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=9500)
